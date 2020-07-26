02ce95574b3aebf2a6cf25bebc453788500a86c6
wl <741598538@qq.com>
2020-07-26 12:41:38 +0800

    【test】

--- test/thread-test/test.c
+++ test/thread-test/test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <linux/delay.h>
 
 
 int main(int argc,char *argv[])
--- test/udp-test/udp_test.c
+++ test/udp-test/udp_test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <net/if.h>
